e4886564e9397e41242bcc4ed9dcbfc6c143ab5d
77f315 <77f315@gmail.com>
2024-10-07 13:47:20 +0300

    db model

---
+++ backend/models.py
@@ -0,0 +1,9 @@
+from flask_sqlalchemy import SQLAlchemy
+db = SQLAlchemy
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    url = db.Column(db.String(500), nullable=False)
+    original_price = db.Column(db.Float, nullable=False)
+    cur_price = db.Column(db.Float, nullable=True)
+    email = db.Column(db.String(100), nullable=False)
