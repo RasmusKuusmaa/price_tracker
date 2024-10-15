599cf9615deb6083dda118f7f1cd1fcaa2a6a95c
77f315 <77f315@gmail.com>
2024-10-15 19:59:12 +0300

    link product to user

--- backend/models.py
+++ backend/models.py
@@ -1,13 +1,18 @@
 from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy() 
 
+user_products = db.Table('user_products',
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id'), primary_key=True),
+    db.Column('product_id', db.Integer, db.ForeignKey('product.id'), primary_key=True)
+)
 class Product(db.Model):
     id = db.Column(db.Integer, primary_key = True, autoincrement=True)
     url = db.Column(db.String(500), nullable=False)
     original_price = db.Column(db.Float, nullable=True)
-    cur_price = db.Column(db.Float, nullable=True)
-    email = db.Column(db.String(100), nullable=False)
+    last_price = db.Column(db.Float, nullable=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 class User(db.Model):
     id = db.Column(db.Integer, primary_key = True, autoincrement=True)
-    user_name = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(100), nullable=False)
     password = db.Column(db.String(50), nullable=False)
+    products = db.relationship('Product', backref='user', lazy=True)
